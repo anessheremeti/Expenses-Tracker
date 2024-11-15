a18a0f6fe822fc4161312fc2a34d72879e448564
ertihoxha5 <ertihoxha874@gmail.com>
2024-11-15 13:09:36 +0100

    Add event listener for DOMContentLoaded and form input validation setup for Contact Us

---
+++ contact-us/contact-us.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("contactForm");
+    const inputs = form.querySelectorAll("input[required], textarea[required]");
+
+});
