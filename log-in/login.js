3f2869bdf774b6b381ca5d5a8fe1d227d5b1ca02
ertihoxha5 <ertihoxha874@gmail.com>
2024-11-26 18:47:51 +0100

    Added event listener for DOMContentLoaded in login validation

---
+++ log-in/login.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("login_form");
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const errorElement = document.getElementById('error');
+    const inputs = form.querySelectorAll("input[required]");
+
+});
