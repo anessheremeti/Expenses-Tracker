28094d95d420180bc9e2697c1ad262dbc0be35fd
EnsarZeqiri <ensarzeqiri15@gmail.com>
2024-11-29 13:27:14 +0100

    funksionet

---
+++ Sing-up/funksionet.js
@@ -0,0 +1,4 @@
+// Kur të gjitha elementet e faqes janë ngarkuar, ekzekuto këtë funksion
+document.addEventListener("DOMContentLoaded", function () {
+    // Gjej formularin dhe ruaje në variablën "form"
+    const form = document.querySelector("form");
