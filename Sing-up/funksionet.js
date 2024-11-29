e3ed3f127c745e420b0bbffc418013cb00dd469b
EnsarZeqiri <ensarzeqiri15@gmail.com>
2024-11-29 13:28:19 +0100

    funksionet

--- Sing-up/funksionet.js
+++ Sing-up/funksionet.js
@@ -2,3 +2,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Gjej formularin dhe ruaje në variablën "form"
     const form = document.querySelector("form");
+    // Shto një event listener për eventin "submit" të formës
+    form.addEventListener("submit", function (e) {
+        // Parandalon që forma të dërgohet automatikisht, për të kryer validimet para se të dërgohet
+         e.preventDefault(); 
+ 
