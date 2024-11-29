c1ddc04d8a46f12e790428f95ff6634d57ee6da3
EnsarZeqiri <ensarzeqiri15@gmail.com>
2024-11-29 13:29:44 +0100

    funksionet

--- Sing-up/funksionet.js
+++ Sing-up/funksionet.js
@@ -6,4 +6,9 @@ document.addEventListener("DOMContentLoaded", function () {
     form.addEventListener("submit", function (e) {
         // Parandalon që forma të dërgohet automatikisht, për të kryer validimet para se të dërgohet
          e.preventDefault(); 
- 
+ // Merr dhe ruan vlerat e fushave të formës, duke hequr hapësirat boshe nga fillimi dhe fundi
+ const fullName = form.elements["full-name"].value.trim();
+ const username = form.elements["name"].value.trim();
+ const email = form.elements["email"].value.trim();
+ const password = form.elements["password"].value;
+ const confirmPassword = form.elements["Config-password"].value;
