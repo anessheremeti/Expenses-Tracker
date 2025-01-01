7d9d8a64147eb03e9b37d4cd20e17851df2518b9
EnsarZeqiri <ensarzeqiri15@gmail.com>
2025-01-01 15:18:41 +0100

    KODI I PHP PER LOGIN DHE SINGUP I PERFUNDUAR 89%

--- log-in/login.php
+++ log-in/login.php
@@ -14,7 +14,7 @@
         <header>
             <h1 class="label">Log In</h1>
         </header>
-        <form class="login_form" id="login_form" action="#">
+        <form class="login_form" id="login_form" action="../register/register.php" method="post">
             <div class="field">
                 <label for="username">Username</label>
                 <input type="text" id="username" name="username" placeholder="Username">
@@ -26,13 +26,13 @@
                 <a href="#">Forgot Password?</a>
             </div>
            
-            <button type="submit" class="button">Log In</button>
+            <button type="submit" class="button" name="login">Log In</button>
     
             <div id="error"></div>
             <p class="signup-text-button">Don't have an account? <a href="../Sing-up/sing-up.php">Sign Up</a></p>
         </form>
     </div>
-    <script src="login.js"></script>
+     <!-- <script src="login.js"></script>  -->
 </body>
 
 </html>
---
+++ register/register.php
@@ -0,0 +1,57 @@
+<?php
+include '../database.php';
+
+if(isset($_POST['signUp'])){
+    echo "Sign Up button clicked<br>";
+    $fulltName=trim($_POST['full-name']);
+    $userName =trim($_POST['name']);   
+    $email =trim($_POST['email']); 
+    $password=trim($_POST['password']);
+  //  $password=md5($password);
+
+    $checkUserName="SELECT * FROM users WHERE name='$userName'";
+    $result=$conn->query($checkUserName);
+    if($result->num_rows > 0){
+        echo "Users Address Already Exists ! ";
+    }
+    else{
+        echo"    aaaaaaaaaaaaa";
+        $insertQuery="INSERT INTO users( fullname ,name,email,password)
+                        VALUES ('$fulltName' , '$userName' , '$email','$password')";
+
+                        if($conn->query( $insertQuery)==TRUE){
+                            header("location: /expense/dashbord/dashbord.php");
+                            exit();
+                        }
+                        else{
+                            echo "Error".$conn->error;
+                        }
+                        
+    }
+}
+
+if(isset($_POST['login'])){
+    $userName = trim($_POST['username']);
+    $password = trim($_POST['password']);
+  //  $password=md5($password);
+    
+    $sql ="SELECT * FROM users WHERE name='$userName' AND password='$password' ";
+
+    $result=$conn->query($sql);
+    if($result->num_rows > 0){
+        session_start();
+        $row=$result->fetch_assoc();
+        $_SESSION['username']=$row['name'];
+       
+        header("Location: /expense/dashbord/dashbord.php");
+        exit();
+    }
+    else{
+        echo "note found incorrect pasword";
+        header("Location: /expense/log-in/login.php");
+       
+        echo $userName;
+        echo $password;
+    }
+}
+?>
